4d8c3fdff02f67d997e639e3d30df6bc53c2b8ea
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-06-05 12:08:27 +0530

    Update variables.tf

--- VPC_terraform/variables.tf
+++ VPC_terraform/variables.tf
@@ -1,9 +1,10 @@
+//Cidr block consists of IP address of VPC to communicate.
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
-
+//Cidr for subnet IP
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
