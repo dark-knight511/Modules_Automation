b2044ceaa03f237b0e7d53321c9f0cbc94b8fadd
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-06-28 17:26:05 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,30 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-}
+resource "aws_route53_record" "this" {
+  zone_id = var.zone_id
+  name    = var.record_name
+  type    = var.record_type
+  ttl     = var.record_ttl
+  records = var.record_values
 
-resource "aws_route53_zone" "example" {
-  name              = var.zone_name
-  
- 
-}
-
-resource "aws_route53_record" "record" {
-  for_each = { for idx, val in var.record_values : idx => val }
-  zone_id  = aws_route53_zone.example.zone_id
-  name     = var.record_name
-  type     = var.record_type
-  ttl      = var.record_ttl
-  records  = [each.value]
-}
-
-resource "aws_route53_record" "alias" {
-  for_each = var.alias_name != null ? { alias = var.alias_name } : {}
-  zone_id  = aws_route53_zone.example.zone_id
-  name     = each.value
-  type     = "A"
-  alias {
-    name                   = each.value
-    zone_id                = var.alias_zone_id
-    evaluate_target_health = var.alias_evaluate_target_health
+  dynamic "alias" {
+    for_each = var.alias_name != "" && var.alias_zone_id != "" ? [1] : []
+    content {
+      name                   = var.alias_name
+      zone_id                = var.alias_zone_id
+      evaluate_target_health = var.alias_evaluate_target_health
+    }
   }
 }
