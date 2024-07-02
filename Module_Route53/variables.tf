6a0ec748595682f4a9a3ee7ef203666c12f66462
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-02 10:48:34 +0530

    Update variables.tf

--- Module_Route53/variables.tf
+++ Module_Route53/variables.tf
@@ -1,28 +1,14 @@
-variable "zone_name" {
-  description = "The domain name for the hosted zone"
+variable "zone_id" {
+  description = "The ID of the hosted zone."
   type        = string
 }
 
-
-variable "record_name" {
-  description = "The name of the record set"
-  type        = string
+variable "route53_records" {
+  description = "A map of Route 53 records."
+  type = map(object({
+    name    = string
+    type    = string
+    ttl     = number
+    records = list(string)
+  }))
 }
-
-variable "record_type" {
-  description = "The DNS record type (e.g., A, AAAA, CNAME)"
-  type        = string
-}
-
-variable "record_ttl" {
-  description = "The time-to-live for the record"
-  type        = number
-  default     = 300
-}
-
-variable "record_values" {
-  description = "A list of resource records"
-  type        = list(string)
-  default     = []
-}
-
