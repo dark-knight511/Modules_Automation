4edc6918002f61b1a1fb6b2152bce3443f4be8f2
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-02 11:19:44 +0530

    Update variables.tf

--- Module_Route53/variables.tf
+++ Module_Route53/variables.tf
@@ -3,10 +3,14 @@ variable "zone_id" {
   type        = string
 }
 
+variable "zone_name" {
+  description = "The name of the hosted zone."
+  type        = string
+}
+
 variable "route53_records" {
   description = "A map of Route 53 records."
   type = map(object({
-    name    = string
     type    = string
     ttl     = number
     records = list(string)
