2697ec22285e7a9a40c0047db39f558a22a272ba
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-03 11:40:22 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -2,10 +2,10 @@ resource "aws_route53_record" "records" {
   for_each = var.route53_records
 
   zone_id = var.zone_id
-  name    = each.value.Name
-  type    = each.value.Type
-  ttl     = each.value.TTL
-  records = each.value.ResourceRecords
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = each.value.ttl
+  records = each.value.records
 
   dynamic "alias" {
     for_each = each.value.alias_name != null && each.value.alias_zone_id != null && each.value.alias_evaluate_target_health != null ? [each.value] : []
