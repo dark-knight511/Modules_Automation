be8722a944dd3c129344b39fd34a7302229e5554
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-03 12:49:52 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,18 +1,19 @@
 locals {
-  record_names = [for key, value in var.route53_records : key]
+  record_names = keys(var.route53_records)
 }
 
 resource "aws_route53_record" "records" {
-  for_each = { for idx, record_name in local.record_names : record_name => var.route53_records[record_name] }
+  for_each = var.route53_records
 
   zone_id = var.zone_id
-  name    = each.key
+  name    = each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
   records = each.value.records
 
   dynamic "alias" {
-    for_each = each.value.alias_name != null && each.value.alias_zone_id != null && each.value.alias_evaluate_target_health != null ? [each.value] : []
+    for_each = can(index(each.value, "alias_name")) && can(index(each.value, "alias_zone_id")) && can(index(each.value, "alias_evaluate_target_health")) ? [each.value] : []
+
     content {
       name                   = alias.value.alias_name
       zone_id                = alias.value.alias_zone_id
