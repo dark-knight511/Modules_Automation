6e392e12c7b444636349ebeae4ea7951f35b29e9
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-03 14:46:53 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,23 +1,36 @@
+
 locals {
-  records = { for k, v in var.route53_records : v.name => "${var.zone_id}.${v.name}" }
+  records = {
+    for key, value in var.route53_records : "${var.zone_id}.${value.name}" => {
+      name    = value.name
+      type    = value.type
+      ttl     = value.ttl
+      records = value.records
+      alias   = {
+        name                   = value.alias_name
+        zone_id                = value.alias_zone_id
+        evaluate_target_health = value.alias_evaluate_target_health
+      }
+    }
+  }
 }
 
 resource "aws_route53_record" "records" {
-  for_each = var.route53_records
+  for_each = local.records
 
   zone_id = var.zone_id
-  name    = local.records[each.key]  # Use local variable records for the name
+  name    = each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
   records = each.value.records
 
   dynamic "alias" {
-    for_each = can(index(each.value, "alias_name")) && can(index(each.value, "alias_zone_id")) && can(index(each.value, "alias_evaluate_target_health")) ? [each.value] : []
+    for_each = can(index(each.value, "alias")) ? [each.value.alias] : []
 
     content {
-      name                   = alias.value.alias_name
-      zone_id                = alias.value.alias_zone_id
-      evaluate_target_health = alias.value.alias_evaluate_target_health
+      name                   = alias.value.name
+      zone_id                = alias.value.zone_id
+      evaluate_target_health = alias.value.evaluate_target_health
     }
   }
 }
