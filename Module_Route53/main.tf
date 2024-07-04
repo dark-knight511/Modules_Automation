b7670ddbe46757b46547636d93254222cad6662e
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-04 16:37:38 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,23 +1,18 @@
 resource "aws_route53_record" "records" {
-  for_each = { for k, v in var.route53_records : k => v if v.alias_name == "" || v.alias_zone_id == "" || v.alias_evaluate_target_health == null }
+  for_each = { for record in var.route53_records : record.name => record }
 
-  zone_id = "Z0465627ZWX"
+  zone_id = "AASEFRF"
   name    = each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
   records = each.value.records
-}
 
-resource "aws_route53_record" "alias_records" {
-  for_each = { for k, v in var.route53_records : k => v if v.alias_name != "" && v.alias_zone_id != "" && v.alias_evaluate_target_health != null }
-
-  zone_id = "Z0465627ZWX"
-  name    = each.value.name
-  type    = each.value.type
-
-  alias {
-    name                   = each.value.alias_name
-    zone_id                = each.value.alias_zone_id
-    evaluate_target_health = each.value.alias_evaluate_target_health
+  dynamic "alias" {
+    for_each = each.value.alias != null ? [each.value.alias] : []
+    content {
+      name                   = alias.value.name
+      zone_id                = alias.value.zone_id
+      evaluate_target_health = alias.value.evaluate_target_health
+    }
   }
 }
