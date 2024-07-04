fcc294ee7b788ca3c42449efb474c4874c44f266
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-04 16:38:41 +0530

    Update variables.tf

--- Module_Route53/variables.tf
+++ Module_Route53/variables.tf
@@ -1,17 +1,15 @@
-variable "zone_id" {
-  description = "The Route 53 hosted zone ID"
-}
-
 variable "route53_records" {
-  description = "Map of Route 53 records"
-  type        = map(object({
+  description = "List of Route 53 records"
+  type = list(object({
     name    = string
     type    = string
     ttl     = number
     records = list(string)
-    alias_name                   = optional(string)
-    alias_zone_id                = optional(string)
-    alias_evaluate_target_health = optional(bool)
+    alias   = optional(object({
+      name                   = string
+      zone_id                = string
+      evaluate_target_health = bool
+    }))
   }))
+  
 }
-
