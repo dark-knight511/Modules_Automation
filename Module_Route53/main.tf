84638c107b6420f1a9960df963385bed3cdf8cff
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-04 16:46:16 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "records" {
   for_each = { for record in var.route53_records : record.name => record }
 
-  zone_id = "AASEFRF"
+  zone_id = var.zone_id
   name    = each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
