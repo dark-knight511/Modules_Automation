281310ca18b179762cb9066400f6945ee28f326a
dark-knight511 <169071593+dark-knight511@users.noreply.github.com>
2024-07-04 15:18:50 +0530

    Update main.tf

--- Module_Route53/main.tf
+++ Module_Route53/main.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "records" {
   for_each = { for k, v in var.route53_records : k => v if v.alias_name == "" || v.alias_zone_id == "" || v.alias_evaluate_target_health == null }
 
-  zone_id = ""
+  zone_id = "Z0465627ZWX"
   name    = each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
@@ -11,7 +11,7 @@ resource "aws_route53_record" "records" {
 resource "aws_route53_record" "alias_records" {
   for_each = { for k, v in var.route53_records : k => v if v.alias_name != "" && v.alias_zone_id != "" && v.alias_evaluate_target_health != null }
 
-  zone_id = ""
+  zone_id = "Z0465627ZWX"
   name    = each.value.name
   type    = each.value.type
 
